11a431081d75b9d43b08bb9e76f1a71b53e02d12
Nora Delaney <nora.delaney@gmail.com>
2015-08-07 10:02:52 -0400

    factorial

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,7 +1,9 @@
-def factorial(num)
-  if num == 0
+def factorial num
+  if num < 0
+    return "You can't take the factorial of a negative number."
+  elsif num <= 1
     1
   else
-  num * factorial(num - 1)
+    num * factorial(num - 1)
   end
 end
